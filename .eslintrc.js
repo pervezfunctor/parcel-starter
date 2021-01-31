1f12a714c675163dece287ce4d35e7df33dd1f04
Pervez Iqbal <pervezfunctor@gmail.com>
2021-01-31 23:41:33 +0530

    dont use recommended typescript react eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,7 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
